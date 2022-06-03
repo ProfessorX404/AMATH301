b1eb6c9a98416279a4b83b4df5230049bed7de98
Xavier <xseggs@gmail.com>
2022-06-03 15:05:24 -0700

    HW8 init

---
+++ Homework8/HW8Code.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# dp/pt, where p(t) is the ratio of dextral
+# (right-handed) snails to the total population.
+
+
+def dp(p):
+    return p * (1 - p)(p - .5)
+
+
+def problem1():
+    pass
+
+
+A1 = problem1()
